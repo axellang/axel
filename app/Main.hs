93c8bc0b248ff17961d9c6602436ab25df4853b8
Joshua Grosso <jgrosso1@asu.edu>
2018-01-25 08:14:24 -0700

    Prototype backquoting

--- app/Main.hs
+++ app/Main.hs
@@ -11,15 +11,15 @@ import Lihsp.AST
   , TopLevel
   , statements
   )
-
 import Lihsp.Error (Error(MacroError))
 import Lihsp.Macros (expandMacros, extractMacroDefinitions)
 import qualified Lihsp.Normalize as Normalize
   ( normalizeExpression
   , normalizeStatement
   )
-import qualified Lihsp.Parse as Parse (Expression)
+import qualified Lihsp.Parse as Parse (Expression(SExpression, Symbol))
 import Lihsp.Parse (runSingle)
+import Lihsp.Utils.Recursion (Recursive(bottomUpFmap))
 
 main :: IO ()
 main = do
@@ -34,7 +34,7 @@ main = do
       \  ((_) (return (list (Literal-int 1) (Literal-int 2) (Literal-int (y 4))))))\
 
       \ (= main (IO Unit)\
-      \  (() (+ 1 (x 1)))))"
+      \  (() (return (+ 1 (x 1))))))"
   result2 <- expansionPass result
   putStrLn $ toHaskell $ stripMacroDefinitions $ normalizeProgram result2
 
@@ -68,3 +68,35 @@ stripMacroDefinitions = statements %~ filter (not . isMacroDefinition)
   where
     isMacroDefinition (SMacroDefinition _) = True
     isMacroDefinition _ = False
+
+-- http://www.lispworks.com/documentation/HyperSpec/Body/02_df.htm
+quote :: Parse.Expression -> Parse.Expression
+quote x = Parse.SExpression [Parse.Symbol "quote", x]
+
+quasiquoteSexp :: Parse.Expression -> [Parse.Expression]
+quasiquoteSexp (Parse.SExpression exprs) =
+  case exprs of
+    [Parse.Symbol "unquote", x] -> [Parse.SExpression [Parse.Symbol "list", x]]
+    Parse.Symbol "unquoteSplicing":xs -> xs
+    _ ->
+      [ Parse.SExpression $
+        Parse.Symbol "append" : concatMap quasiquoteSexp exprs
+      ]
+quasiquoteSexp x = [quote x]
+
+quasiquote :: Parse.Expression -> Parse.Expression
+quasiquote = head . quasiquoteSexp
+
+expandQuasiquote :: Parse.Expression -> Parse.Expression
+expandQuasiquote =
+  bottomUpFmap $ \expr ->
+    case expr of
+      Parse.SExpression [Parse.Symbol "quasiquoteAtom", x] ->
+        case x of
+          Parse.SExpression _ -> x
+          _ -> quasiquote x
+      Parse.SExpression [Parse.Symbol "quasiquoteSexp", x] ->
+        case x of
+          Parse.SExpression _ -> quasiquote x
+          _ -> x
+      _ -> expr
