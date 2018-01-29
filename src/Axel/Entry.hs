ff4c6c04418eac806eb5ae72ef015c0e5ca9b475
Joshua Grosso <jgrosso1@asu.edu>
2018-01-29 16:57:51 -0700

    Sandbox macros in temp directory

--- src/Axel/Entry.hs
+++ src/Axel/Entry.hs
@@ -3,19 +3,30 @@
 
 module Axel.Entry where
 
-import Control.Lens.Operators ((.~))
+import Control.Lens.Operators ((.~), (^.))
+import Control.Monad (when)
 import Control.Monad.Except (MonadError, runExceptT)
 import Control.Monad.IO.Class (MonadIO)
 
 import Axel.AST (ToHaskell(toHaskell))
 import Axel.Error (Error)
-import Axel.Eval (evalSource)
-import Axel.Macros (exhaustivelyExpandMacros, stripMacroDefinitions)
+import Axel.Eval (execInterpreter)
+import Axel.Macros
+  ( exhaustivelyExpandMacros
+  , getAstDefinition
+  , stripMacroDefinitions
+  )
 import Axel.Normalize (normalizeStatement)
 import Axel.Parse (Expression(Symbol), parseSource)
 import Axel.Utils.Recursion (Recursive(bottomUpFmap))
 
-import System.FilePath.Lens (extension)
+import System.Directory
+  ( createDirectory
+  , doesDirectoryExist
+  , removeDirectoryRecursive
+  )
+import System.FilePath ((</>))
+import System.FilePath.Lens (directory, extension)
 
 convertList :: Expression -> Expression
 convertList =
@@ -36,6 +47,7 @@ transpileSource source =
   (parseSource source >>= exhaustivelyExpandMacros . convertList . convertUnit >>=
    normalizeStatement)
 
+-- TODO Switch this to `(MonadError Error m, MonadIO m)` and do the error check in `evalFile`.
 transpileFile :: FilePath -> FilePath -> IO ()
 transpileFile path newPath =
   readFile path >>= runExceptT . transpileSource >>= \case
@@ -44,10 +56,18 @@ transpileFile path newPath =
 
 evalFile :: FilePath -> IO ()
 evalFile path = do
-  let newPath = extension .~ ".hs" $ path
+  let newPath = directory .~ tempDirectoryPath $ extension .~ "hs" $ path
+  let astDefinitionPath = (newPath ^. directory) </> "Axel.hs"
+  ensureCleanTempDirectory
+  getAstDefinition >>= writeFile astDefinitionPath
   transpileFile path newPath
-  newContents <- readFile newPath
-  evalResult <- runExceptT $ evalSource newContents
+  evalResult <- runExceptT $ execInterpreter newPath
   case evalResult of
     Left err -> print err
     Right res -> putStrLn res
+  where
+    tempDirectoryPath = (path ^. directory) </> "axelTemp"
+    ensureCleanTempDirectory = do
+      tempDirectoryExists <- doesDirectoryExist tempDirectoryPath
+      when tempDirectoryExists $ removeDirectoryRecursive tempDirectoryPath
+      createDirectory tempDirectoryPath
