ef79f85e2cb2f434db88e7e6f672be6cd122bdeb
Joshua Grosso <jgrosso256@gmail.com>
2018-07-15 15:53:35 -0700

    Add wrappers for `stack build` and `stack exec <project>-exe`

--- src/Axel/Project.hs
+++ src/Axel/Project.hs
@@ -6,7 +6,9 @@ import Axel.Entry (transpileFile')
 import Axel.Utils.Directory (getRecursiveContents)
 
 import Control.Monad (void)
+import Control.Monad.Except (throwError)
 
+import Data.List (foldl')
 import Data.Semigroup ((<>))
 import qualified Data.Text as T (isSuffixOf, pack)
 
@@ -14,7 +16,9 @@ import Paths_axel (getDataFileName)
 
 import System.Directory (copyFile, getCurrentDirectory, removeFile)
 import System.FilePath ((</>))
-import System.Process (readProcess)
+import System.Process (readProcess, readProcessWithExitCode)
+
+import Text.Regex.PCRE ((=~), getAllTextSubmatches)
 
 newProject :: String -> IO ()
 newProject projectName = do
@@ -29,10 +33,32 @@ newProject projectName = do
         removeFile (projectDirectory </> filePath <> ".hs")
   mapM_ copyAxel ["Setup", "app" </> "Main", "src" </> "Lib", "test" </> "Spec"]
 
-transpileProject :: IO ()
+transpileProject :: IO [FilePath]
 transpileProject = do
   files <- getRecursiveContents "."
   let axelFiles =
         filter (\filePath -> ".axel" `T.isSuffixOf` T.pack filePath) files
-  newPaths <- mapM transpileFile' axelFiles
-  mapM_ removeFile newPaths
+  mapM transpileFile' axelFiles
+
+buildProject :: IO ()
+buildProject = do
+  hsPaths <- transpileProject
+  void $ readProcess "stack" ["build"] ""
+  mapM_ removeFile hsPaths
+
+runProject :: IO ()
+runProject = do
+  (_, _, stderr) <- readProcessWithExitCode "stack" ["ide", "targets"] ""
+  let targets = lines stderr
+  case findExeTargets targets of
+    [target] -> readProcess "stack" ["exec", target] "" >>= putStr
+    _ -> throwError (userError "No executable target was unambiguously found!")
+  where
+    findExeTargets =
+      foldl'
+        (\acc target ->
+           case getAllTextSubmatches $
+                target =~ ("([^:]*):exe:([^:]*)" :: String) of
+             [_fullMatch, _projectName, targetName] -> targetName : acc
+             _ -> acc)
+        []
