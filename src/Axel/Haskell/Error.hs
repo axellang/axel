dc9d83354fdedcb8af4e973e0b894cd2fd3a41f4
Joshua Grosso <jgrosso256@gmail.com>
2019-08-30 18:26:01 -0700

    When a Haskell error can't be parsed, show the original error location

--- src/Axel/Haskell/Error.hs
+++ src/Axel/Haskell/Error.hs
@@ -10,6 +10,7 @@ import Axel.Haskell.FilePath (haskellPathToAxelPath)
 import Axel.Macros (ModuleInfo)
 import Axel.Sourcemap (SourcePosition(SourcePosition), renderSourcePosition)
 import qualified Axel.Sourcemap as SM (Output(Output), findOriginalPosition)
+import Axel.Utils.Debug
 import Axel.Utils.Json (_Int)
 
 import Control.Lens.Operators ((^.), (^?))
@@ -43,57 +44,51 @@ makeFieldsNoPrefix ''GhcError
 
 processErrors :: ModuleInfo -> String -> String
 processErrors moduleInfo stackOutput =
-  unlines $ map (processStackOutputLine moduleInfo) $ lines stackOutput
+  unlines $ concatMap (processStackOutputLine moduleInfo) $ lines stackOutput
 
-processStackOutputLine :: ModuleInfo -> String -> String
+processStackOutputLine :: ModuleInfo -> String -> [String]
 processStackOutputLine moduleInfo line =
-  fromMaybe line (tryProcessGhcOutput moduleInfo line)
+  fromMaybe [line] (tryProcessGhcOutput moduleInfo line)
 
-tryProcessGhcOutput :: ModuleInfo -> String -> Maybe String
+tryProcessGhcOutput :: ModuleInfo -> String -> Maybe [String]
 tryProcessGhcOutput moduleInfo line = do
   let obj `viewStr` field = T.unpack <$> (obj ^? field . _String)
   jsonLine <- Json.decode' @Json.Value (BL.pack line)
   msg <- jsonLine `viewStr` key "doc"
-  let maybeAxelError = do
-        sourceSpan <-
-          do jsonSpan <- jsonLine ^? key "span"
-             startPosition <-
-               SourcePosition <$> (jsonSpan ^? key "startLine" . _Int) <*>
-               (jsonSpan ^? key "startCol" . _Int)
-             endPosition <-
-               SourcePosition <$> (jsonSpan ^? key "endLine" . _Int) <*>
-               (jsonSpan ^? key "endCol" . _Int)
-             filePath <- jsonSpan `viewStr` key "file"
-             pure (filePath, SourceSpan startPosition endPosition)
-        toAxelError moduleInfo $ GhcError msg sourceSpan
-  pure $ fromMaybe msg maybeAxelError
+  jsonSpan <- jsonLine ^? key "span"
+  startPosition <-
+    SourcePosition <$> (jsonSpan ^? key "startLine" . _Int) <*>
+    (jsonSpan ^? key "startCol" . _Int)
+  endPosition <-
+    SourcePosition <$> (jsonSpan ^? key "endLine" . _Int) <*>
+    (jsonSpan ^? key "endCol" . _Int)
+  filePath <- jsonSpan `viewStr` key "file"
+  let sourceSpan = SourceSpan startPosition endPosition
+  let maybeAxelError =
+        toAxelError moduleInfo $ GhcError msg (filePath, sourceSpan)
+  let haskellError =
+        "\n\nAt position " <> filePath <> ":" <>
+        renderSourcePosition startPosition <>
+        ":\n" <>
+        msg <>
+        "\n\n"
+  pure $ [fromMaybe haskellError maybeAxelError]
 
 toAxelError :: ModuleInfo -> GhcError -> Maybe String
 toAxelError moduleInfo ghcError = do
-  let axelPath = haskellPathToAxelPath $ ghcError ^. transpiledSpan . _1
+  let haskellPath = ghcError ^. transpiledSpan . _1
+  let haskellPosition = ghcError ^. transpiledSpan . _2 . start
+  let axelPath = haskellPathToAxelPath haskellPath
+  let positionHint filePath startPos = do
+        pure $ "at " <> filePath <> ":" <> renderSourcePosition startPos
+  haskellPositionHint <- positionHint haskellPath haskellPosition
   SM.Output transpiledOutput <- M.lookup axelPath moduleInfo >>= snd
-  let findPosition field =
-        SM.findOriginalPosition
-          transpiledOutput
-          (ghcError ^. transpiledSpan . _2 . field)
-  origStartPosition <- findPosition start >>= id
-  maybeOrigEndPosition <- findPosition end
-  let positionNoRangeHint =
-        "at position " <> renderSourcePosition origStartPosition
-  let positionHint =
-        case maybeOrigEndPosition of
-          Just origEndPosition ->
-            let positionRangeHint =
-                  "from position " <> renderSourcePosition origStartPosition <>
-                  " to position " <>
-                  renderSourcePosition origEndPosition
-             in if origStartPosition == origEndPosition
-                  then positionNoRangeHint
-                  else positionRangeHint
-          Nothing -> positionNoRangeHint
-  pure $ "\n\n\n\n" <> ghcError ^. message <>
-    "\n\nThe above message is in terms of the generated Haskell.\nTry checking the file '" <>
-    axelPath <>
-    "', " <>
-    positionHint <>
+  axelPosition <-
+    SM.findOriginalPosition transpiledOutput haskellPosition >>= id
+  axelPositionHint <- positionHint axelPath axelPosition
+  pure $ "\n\n" <> ghcError ^. message <>
+    "\n\nThe above message is in terms of the generated Haskell, " <>
+    haskellPositionHint <>
+    ".\nTry checking " <>
+    axelPositionHint <>
     ".\nIf the Axel code at that position doesn't seem related, something may have gone wrong during a macro expansion.\n\n"
